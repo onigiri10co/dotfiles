c2d2f847882873abc72478f51c989b4da947c0d0
onigiri10co <onigiri10co@gmail.com>
2021-09-05 12:27:13 +0900

    installer | feat: add install.sh
    Run directly from url

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ onigri10co's dotfiles
 
 ## Installation
 ```
+$ curl -fsSL https://raw.githubusercontent.com/onigiri10co/dotfiles/HEAD/install.sh | bash
+
+# another way
 $ git clone https://github.com/onigiri10co/dotfiles.git
 $ cd dotfiles
 $ make install-all
---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+
+DOTF_REPOSITORY='github.com/onigiri10co/dotfiles';
+DOTF_BRANCH="${DOTF_BRANCH:-main}";
+DOTF_WORKPATH=`mktemp -d`
+
+
+finally() {
+  rm -rf "$DOTF_WORKPATH"
+}
+
+exception() {
+  echo "[ERROR] Aborted the Installation because it received one of the following signals: 1 2 3 15.";
+  finally
+  exit 1
+}
+
+main() {
+  curl -fsSL https://${DOTF_REPOSITORY}/archive/${DOTF_BRANCH}.tar.gz -# | tar xz -C ${DOTF_WORKPATH} --strip=1
+  ( 
+    cd ${DOTF_WORKPATH}
+    make install-all
+  )
+}
+
+
+trap finally 0
+trap exception 1 2 3 15
+
+main
