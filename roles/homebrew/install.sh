f87308dec7d5311341d409734380c032e26ae854
onigiri10co <onigiri10co@gmail.com>
2021-09-05 12:29:17 +0900

    installer | feat: add .zshrc for homebrew
    NOTE: .zsh.d/homebrew.zshrc ファイルは、他の同様のファイルとともに、最終的に1枚の .zshrc にまとめる。

--- roles/homebrew/install.sh
+++ roles/homebrew/install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -e
 
 
+readonly CURRENT_PATH=$(cd $(dirname $0); pwd)
+
+
 # Install homebrew
 type brew > /dev/null 2>&1 || {
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
+
+( 
+cd $CURRENT_PATH
+cp -fr .zsh.d "$HOME/"
+)
 
