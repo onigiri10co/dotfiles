4bc8ef85389c028c88a131e3d364aa5243db4afe
onigiri10co <onigiri10co@gmail.com>
2021-09-06 21:31:14 +0900

    installer | feat: add display logo

--- install.sh
+++ install.sh
@@ -1,10 +1,26 @@
 #!/bin/bash -e
 
-
 DOTF_REPOSITORY='github.com/onigiri10co/dotfiles';
 DOTF_BRANCH="${DOTF_BRANCH:-main}";
 DOTF_WORKPATH=`mktemp -d`
 
+
+display_logo() {
+cat <<EOF
+      ───────────────╔╗╔═══╗────────────╔╗──╔╗╔═╗╔╗
+      ──────────────╔╝║║╔═╗║─────╔╗─────║║─╔╝╚╣╔╝║║
+      ╔══╦═╗╔╦══╦╦═╦╬╗║║║║║╠══╦══╣╠══╗╔═╝╠═╩╗╔╝╚╦╣║╔══╦══╗
+      ║╔╗║╔╗╬╣╔╗╠╣╔╬╣║║║║║║║╔═╣╔╗╠╣══╣║╔╗║╔╗║╠╗╔╬╣║║║═╣══╣
+      ║╚╝║║║║║╚╝║║║║╠╝╚╣╚═╝║╚═╣╚╝║╠══║║╚╝║╚╝║╚╣║║║╚╣║═╬══║
+      ╚══╩╝╚╩╩═╗╠╩╝╚╩══╩═══╩══╩══╝╚══╝╚══╩══╩═╩╝╚╩═╩══╩══╝
+      ───────╔═╝║
+      ───────╚══╝
+
+
+      See also: https://$DOTF_REPOSITORY
+
+EOF
+}
 
 finally() {
   rm -rf "$DOTF_WORKPATH"
@@ -17,6 +33,7 @@ exception() {
 }
 
 main() {
+  display_logo
   curl -fsSL https://${DOTF_REPOSITORY}/archive/${DOTF_BRANCH}.tar.gz -# | tar xz -C ${DOTF_WORKPATH} --strip=1
   ( 
     cd ${DOTF_WORKPATH}
