e362c1f14d04ed01b4362a7275034e5b9d49bab9
onigiri10co <onigiri10co@gmail.com>
2021-09-29 20:10:16 +0900

    installer | feat: add ssh role

---
+++ roles/ssh/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+set -e
+
+readonly CURRENT_PATH=$(cd $(dirname $0); pwd)
+
+
+( 
+cd ${CURRENT_PATH}
+cp -fr bin ${HOME}
+chmod +x ${HOME}/bin/*
+)
+
+ln -fs ~/Dropbox/.ssh $HOME
+
