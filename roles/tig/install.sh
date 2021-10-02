a172a8c127fbe495ee7c9bf7569e5aa6afaab2c0
onigiri10co <onigiri10co@gmail.com>
2021-10-02 19:28:47 +0900

    installer | feat: add tig role

---
+++ roles/tig/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+set -e
+
+readonly CURRENT_PATH=$(cd $(dirname $0); pwd)
+
+
+# Install tig
+brew list tig > /dev/null 2>&1 || {
+  brew install tig
+}
+
+( 
+cd ${CURRENT_PATH}
+cp -f .tigrc ${HOME}
+)
+
