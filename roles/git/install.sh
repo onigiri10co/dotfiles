15b0cf78b8eced772d46053ee66fefb737d1b94b
onigiri10co <onigiri10co@gmail.com>
2021-10-03 14:20:45 +0900

    installer | feat: change dir: /.git-templates/.config\/git/

--- roles/git/install.sh
+++ roles/git/install.sh
@@ -3,9 +3,9 @@ set -e
 
 readonly CURRENT_PATH=$(cd $(dirname $0); pwd)
 
+GIT_CONFIG_ROOT_DIR="$HOME/.config/git"
 GIT_USERNAME="${GIT_USERNAME:-onigiri10co}"
 GIT_EMAIL="${GIT_EMAIL:-onigiri10co@gmail.com}"
-GIT_TEMPLATE_ROOT="$HOME/.git-templates"
 
 
 # Install git
@@ -13,25 +13,30 @@ brew list git > /dev/null 2>&1 || {
   brew install git
 }
 
+# Global Git Profile
 git config --global user.name "${GIT_USERNAME}"
 git config --global user.email "${GIT_EMAIL}"
 
-GIT_IGNORE="${GIT_TEMPLATE_ROOT}/.gitignore_global"
+
+mkdir -p ${GIT_CONFIG_ROOT_DIR}
+
+# Global Git ignore
+GIT_IGNORE="${GIT_CONFIG_ROOT_DIR}/.gitignore_global"
 curl -sL https://raw.githubusercontent.com/github/gitignore/master/Global/macOS.gitignore > "${GIT_IGNORE}"
+( cd ${CURRENT_PATH}; cat .gitignore_ownrule >> "${GIT_IGNORE}")
 git config --global core.excludesfile "${GIT_IGNORE}"
 
+# Global Git Commit
 GIT_COMMIT_FILE=".gitcommit_global"
-GIT_COMMIT="${GIT_TEMPLATE_ROOT}/${GIT_COMMIT_FILE}"
-( cd ${CURRENT_PATH}; cp "${GIT_COMMIT_FILE}" "${GIT_TEMPLATE_ROOT}/")
+GIT_COMMIT="${GIT_CONFIG_ROOT_DIR}/${GIT_COMMIT_FILE}"
+( cd ${CURRENT_PATH}; cp "${GIT_COMMIT_FILE}" "${GIT_CONFIG_ROOT_DIR}/")
 git config --global commit.template "${GIT_COMMIT}"
 
-
-# Install git-secrets
+# Global Git Secret
 brew list git-secrets > /dev/null 2>&1 || {
   brew install git-secrets
 }
-
-GIT_SECRETS="${GIT_TEMPLATE_ROOT}/git-secrets"
+GIT_SECRETS="${GIT_CONFIG_ROOT_DIR}/git-secrets"
 git secrets --install ${GIT_SECRETS} --force > /dev/null
 git config --global init.templatedir ${GIT_SECRETS}
 
