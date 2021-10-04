5919ce8bf8a4cbedfbd4cf8c7d180b9d41f56819
onigiri10co <onigiri10co@gmail.com>
2021-10-04 18:50:20 +0900

    installer | feat: add cd ghq dir
    shell 内で、cd しても出来ないので、alias で source している。
    Ref. https://stackoverflow.com/questions/255414/why-cant-i-change-directories-using-cd-in-a-script

--- roles/ghq/install.sh
+++ roles/ghq/install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 set -e
 
+readonly CURRENT_PATH=$(cd $(dirname $0); pwd)
+
 
 # Install ghq
 brew list ghq > /dev/null 2>&1 || {
@@ -11,4 +13,11 @@ brew list ghq > /dev/null 2>&1 || {
 # [ghq]
 # 	root = ~/src
 git config --global ghq.root '~/src'
+
+( 
+cd ${CURRENT_PATH}
+cp -fr .zsh.d ${HOME}
+cp -fr bin ${HOME}
+chmod +x ${HOME}/bin/*
+)
 
