fdbb2a8ca4d58ba1a8e5fc1eed2fa9bfd8b1cf73
onigiri10co <onigiri10co@gmail.com>
2022-03-08 19:04:18 +0900

    feat: add poetry role
    virtualenvs.in-project true: poetry が作成する仮想環境（.venv）をプロジェクト直下に配置する。
    わかり易いのと、VSCode 使うときに、こうしておくとこれを参照してくれるらしい。など、メリットが多いので。

---
+++ roles/poetry/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+set -e
+
+readonly CURRENT_PATH=$(cd $(dirname $0); pwd)
+
+
+pipx list | grep poetry > /dev/null 2>&1 || {
+  pipx install poetry
+}
+
+# Ref. https://python-poetry.org/docs/configuration/#virtualenvsin-project
+poetry config virtualenvs.in-project true 
+
