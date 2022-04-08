a7825c193c9df72a7ca8269f25a4cc7b784529c7
onigomex <onigomex+github@gmail.com>
2022-04-08 17:21:51 +0900

    feat: Add awscli role
    use asdf

---
+++ roles/awscli/README.md
@@ -0,0 +1,3 @@
+# roles/awscli
+[MetricMike/asdf-awscli](https://github.com/MetricMike/asdf-awscli)
+
---
+++ roles/awscli/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+set -e
+
+readonly CURRENT_PATH=$(cd $(dirname $0); pwd)
+
+
+asdf plugin list awscli > /dev/null 2>&1 || {
+  asdf plugin add awscli
+}
+
+asdf install awscli latest
+asdf global awscli latest
+
