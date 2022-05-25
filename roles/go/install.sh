7efe94807cee0a0f444a47a4166279399a249b40
onigomex <onigomex+github@gmail.com>
2022-05-25 13:50:37 +0900

    feat(go): Revert commit to install by brew
    やっぱり、過去バージョン使いたい場合は、公式の方法で対応することにした。
    なので、asdf は不要となった。

--- roles/go/install.sh
+++ roles/go/install.sh
@@ -4,12 +4,9 @@ set -e
 readonly CURRENT_PATH=$(cd $(dirname $0); pwd)
 
 
-asdf list golang > /dev/null 2>&1 || {
-  asdf plugin add golang
+brew list go > /dev/null 2>&1 || {
+  brew install go
 }
-
-asdf install golang latest
-asdf global golang latest
 
 (
 cd ${CURRENT_PATH}
