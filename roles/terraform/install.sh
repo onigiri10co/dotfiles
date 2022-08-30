8c937d3ddf1236adf3a79c2e6758bc18de5f9dbc
onigomex <onigomex+github@gmail.com>
2022-08-30 20:51:25 +0900

    feat(terraform): Add tflint, tfsec, terraformer

--- roles/terraform/install.sh
+++ roles/terraform/install.sh
@@ -11,3 +11,19 @@ asdf list terraform > /dev/null 2>&1 || {
 asdf install terraform latest
 asdf global terraform latest
 
+
+brew list terraformer > /dev/null 2>&1 || {
+  brew install terraformer
+}
+
+brew list tflint > /dev/null 2>&1 || {
+  brew install tflint
+}
+
+brew list tfsec > /dev/null 2>&1 || {
+  brew install tfsec
+}
+
+brew list terraformer > /dev/null 2>&1 || {
+  brew install terraformer
+}
