9081b4ec78170279c99ec5ce9dc8caf1b0ebedd5
onigomex <onigomex+github@gmail.com>
2023-12-29 11:22:34 +0900

    feat(create): Update create/install.sh
    example として、icloud drive からのリンク設定するコードを追加した。

--- scripts/templates/create/install.sh
+++ scripts/templates/create/install.sh
@@ -40,3 +40,16 @@ readonly CURRENT_PATH=$(cd $(dirname $0); pwd)
 # cd ${CURRENT_PATH}
 # cp -fr .zsh.d ${HOME}
 # )
+
+# link(){
+#   ln -fs $HOME/Library/Mobile\ Documents/com\~apple\~CloudDocs/userdata/aws $HOME/.aws
+# }
+#
+# if [ -L $HOME/.aws ]; then
+#   unlink $HOME/.aws
+#   link
+# fi
+#
+# if [ ! -e $HOME/.aws ]; then
+#   link
+# fi
