e4d2bcf7fa9e3820ce128406b2181f833c571db0
onigomex <onigomex+github@gmail.com>
2024-01-03 00:16:56 +0900

    feat: macos-dock を macos にマージ

--- roles/macos/README.md
+++ roles/macos/README.md
@@ -8,6 +8,22 @@ mocOS settings
 
 
 
+## Settings
+### Desktop & Dock
+System Settings > Desktop & Dock
+
+- Dock > Size: 25%
+- Dock > Magnification: Off
+- Position on screen: Left
+- Minimize window using: Genie Effect
+- Double-click a window's title bar to: Zoom
+- Automatically hide and show the Dock: true
+- Animate opening applications: false
+- Show indicators for open applications: false
+- Show recent applications in Dock: false
+
+
+
 ## References
 - [macOSでの各種設定を自動化するdefaultsコマンド - memo.yammer.jp](https://memo.yammer.jp/posts/macos-defaults)
 
--- roles/macos/install.sh
+++ roles/macos/install.sh
@@ -2,7 +2,31 @@
 set -e
 
 
-# .DS_Store ファイルを作らせない
+# System Settings > Desktop & Dock
+## Dock > Size: 25%
+defaults write com.apple.dock tilesize -int 35
+## Dock > Magnification: Off
+defaults write com.apple.dock magnification -bool false
+## Position on screen: Left
+defaults write com.apple.dock orientation -string "left"
+## Minimize window using: Genie Effect
+defaults write com.apple.dock mineffect -string "genie"
+## Double-click a window's title bar to:  Zoom
+defaults write -g AppleActionOnDoubleClick -string "Maximize"
+## Automatically hide and show the Dock: true
+defaults write com.apple.dock autohide -bool true
+## Animate opening applications: false
+defaults write com.apple.dock launchanim -bool false
+## Show indicators for open applications: false
+defaults write com.apple.dock show-process-indicators -bool false
+## Show recent applications in Dock: false
+defaults write com.apple.dock show-recents -bool false
+## Dockからすべてのアプリを消す
+defaults write com.apple.dock persistent-apps -array
+
+
+# Others
+## .DS_Store ファイルを作らせない
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
