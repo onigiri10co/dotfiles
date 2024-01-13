146ca7acc328bf39a51959a953a8c33876a08420
onigomex <onigomex+github@gmail.com>
2024-01-13 22:01:37 +0900

    feat(wezterm): Add `audible_bell = "Disabled"` to config

--- roles/wezterm/.config/wezterm/wezterm.lua
+++ roles/wezterm/.config/wezterm/wezterm.lua
@@ -7,6 +7,7 @@ return {
   color_scheme = "nord", -- See: https://wezfurlong.org/wezterm/colorschemes/index.html
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
+  audible_bell = "Disabled",
 --	disable_default_key_bindings = true,
 }
 
