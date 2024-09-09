ff7b2f479fd69c02ee078f2f5bf8cdf01234ce5c
onigomex <onigomex+github@gmail.com>
2024-09-09 17:39:27 +0900

    feat(nvim): Add close windown keymap, with autocmd, augroup
    vim の以下を nvim に置き換えた。
    
    "Setting/Help
    set helplang=ja
    augroup Help
      autocmd!
      autocmd FileType help call s:help_keymap()
    augroup END
    
    function! s:help_keymap() abort
      nnoremap <silent><buffer> <Leader><Leader> :<C-u>bd<CR>
    endfunction
    
    point:
    - group = augroup('Help', { clear = true }),
      - clear = true は、autocmd! に相当する
    - vim.keymap.set('n', '<Leader><Leader>', ':bd<CR>', { buffer = true, silent = true })
      - buffer = true は、<buffer>、silent = true は、<silent> に相当する。
    
    refs.
    - https://uhoho.hatenablog.jp/entry/2023/05/18/063603
    - https://uhoho.hatenablog.jp/entry/2023/05/19/124317
    - https://www.reddit.com/r/neovim/comments/xhtr1p/nvim_autocmd_filetype_option/

--- roles/nvim/.config/nvim/lua/keymaps.lua
+++ roles/nvim/.config/nvim/lua/keymaps.lua
@@ -1,8 +1,27 @@
 -- ref. https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+local augroup = vim.api.nvim_create_augroup -- Create/get sutocommand group
+local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
 -- Leader
 vim.g.mapleader = " " -- <leader> = space
 vim.g.maplocalleader = "," -- <localLeader>
 
 -- Search
-vim.keymap.set('n', '<ESC><ESC>', ':nohlsearch<CR>', { silent = true })
+vim.keymap.set('n', '<esc><esc>', '<cmd>nohlsearch<cr>', { silent = true })
+
+-- Close Window
+local close_window = function()
+  vim.keymap.set('n', '<leader><leader>', '<cmd>bd<cr>', { buffer = true, silent = true })
+end
+autocmd("FileType", {
+  desc = 'Close "Help" window if you press <leader><leader>',
+  group = augroup('Help', { clear = true }),
+  pattern = 'help',
+  callback = close_window,
+})
+autocmd('FileType', {
+  desc = 'Close "QuickFix" window if you press <leader><leader>',
+  group = augroup('QuickFix', { clear = true }),
+  pattern = 'qf',
+  callback = close_window,
+})
