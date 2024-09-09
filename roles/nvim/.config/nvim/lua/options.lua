2ea53c763f26796ead7f5dfddb17cb9105059d95
onigomex <onigomex+github@gmail.com>
2024-09-09 18:11:43 +0900

    feat(nvim): Update options.lua
    refs.
    - https://zenn.dev/link/comments/21410d7f4f10b3
    - https://qiita.com/tmsanrinsha/items/d6c11f2b7788eb24c776#set-list

--- roles/nvim/.config/nvim/lua/options.lua
+++ roles/nvim/.config/nvim/lua/options.lua
@@ -12,9 +12,17 @@ opt.clipboard = "unnamedplus" -- Sync with system clipboard
 opt.confirm = true -- 保存されていないファイルを q するときに保存の確認を行う
 opt.helplang = 'ja' -- Help に表示する言語
 
+-- Search
+opt.ignorecase = true -- 大文字小文字を区別しない
+opt.smartcase = true -- パターンに大文字が含まれているときに限り、大文字と小文字を区別
+
 -- View
 opt.number = true -- 行番号を表示する
 api.nvim_win_set_option(0, 'signcolumn', 'yes:1') -- サインカラムを常に表示する
+opt.cursorline = true -- 現在行をハイライト
+opt.cursorcolumn = true -- 現在列をハイライト
+opt.list = true -- 改行文字表示
+opt.listchars = 'tab:»-,eol:↴,trail:･,nbsp:⍽,extends:»,precedes:«' -- 改行などの文字を指定
 
 -- Edit
 opt.expandtab = true -- true の場合、タブキー入力時にタブの代わりに半角スペースを入力する
