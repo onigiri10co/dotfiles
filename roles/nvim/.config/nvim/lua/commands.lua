2ab3e493a971c7161e510b4f8ff43235605c6e62
onigomex <onigomex+github@gmail.com>
2024-09-11 21:20:37 +0900

    refactor(nvim): Change single quote from double quote

--- roles/nvim/.config/nvim/lua/commands.lua
+++ roles/nvim/.config/nvim/lua/commands.lua
@@ -1,13 +1,14 @@
 -- ref. https://neovim.io/doc/user/map.html#user-commands
 -- Example
--- vim.api.nvim_create_user_command("OpenHoge", function(opts)
--- 	vim.cmd("e " .. "~/hoge.md")
+-- vim.api.nvim_create_user_command('OpenHoge', function(opts)
+-- 	vim.cmd('e ' .. '~/hoge.md')
 -- end, {})
 
 -- Close vim with qa! command
-vim.api.nvim_create_user_command("Q", function(opts)
-  vim.cmd("qa!")
+vim.api.nvim_create_user_command('Q', function(opts)
+  vim.cmd('qa!')
 end, {})
+
 -- Open the terminal in a split window at the bottom
 vim.api.nvim_create_user_command('T', function(opts)
   vim.cmd('split | wincmd j | resize 20 | terminal')
