a5e4ecbbf89b0006d8b79bd22501cc8d3e06395e
onigomex <onigomex+github@gmail.com>
2024-09-13 16:36:27 +0900

    feat(nvim): Update keymap

--- roles/nvim/.config/nvim/lua/keymaps.lua
+++ roles/nvim/.config/nvim/lua/keymaps.lua
@@ -28,4 +28,6 @@ autocmd('FileType', {
 
 -- Terminal
 -- https://qiita.com/delphinus/items/aea16e82de2145d2a6b7
-vim.keymap.set('t', '<esc>', '<c-\\><c-n>', { desc = '::Keymap Change Terminal-Normal mode by <esc> from Terminal-Job mode. Note: "t" is :tnoremap (Terminal-Job mode)' })
+-- Note: "t" is :tnoremap (Terminal-Job mode)
+vim.keymap.set('t', '<esc>', '<c-\\><c-n>', { desc = '::Keymap Change Terminal-Normal mode by <esc> from Terminal-Job mode.' })
+vim.keymap.set('t', 'jj', '<c-\\><c-n><c-w><c-p>', { desc = '::Keymap Switch back to last window from Terminal-Job mode.' })
