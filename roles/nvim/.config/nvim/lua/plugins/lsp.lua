9d4377558ea5286ab392f57b8e29a3a7dbebde1c
onigomex <onigomex+github@gmail.com>
2024-09-18 19:01:39 +0900

    feat(nvim): Add RRethy/vim-illuminate
    同じ変数をハイライトする。

--- roles/nvim/.config/nvim/lua/plugins/lsp.lua
+++ roles/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -109,6 +109,24 @@ return {
     end
   },
 
+  -- https://github.com/RRethy/vim-illuminate
+  {
+    "RRethy/vim-illuminate",
+    config = function()
+      require 'illuminate'.configure({
+        vim.api.nvim_create_autocmd("BufReadPost", {
+          group = vim.api.nvim_create_augroup("Change_Illuminate", { clear = true }),
+          callback = function()
+            vim.cmd([[hi IlluminatedWordText cterm=underline gui=undercurl]])
+            vim.cmd([[hi IlluminatedWordRead cterm=underline gui=undercurl]])
+            vim.cmd([[hi IlluminatedWordWrite cterm=underline gui=undercurl]])
+          end,
+          desc = "Change Illuminate Highlights",
+        })
+      })
+    end
+  },
+
   -- Autocompletion plugin
   -- https://github.com/hrsh7th/nvim-cmp/
   {
