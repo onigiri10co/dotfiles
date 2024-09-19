1649bf71b486f54a4bf1c3438fc86abe2ec332f5
onigomex <onigomex+github@gmail.com>
2024-09-19 15:05:43 +0900

    feat(nvim): Add hop plugin(like easy-motion)

--- roles/nvim/.config/nvim/lua/plugins/ui.lua
+++ roles/nvim/.config/nvim/lua/plugins/ui.lua
@@ -4,4 +4,14 @@ return {
     -- https://github.com/simeji/winresizer
     "simeji/winresizer",
   },
+
+  -- https://github.com/smoka7/hop.nvim
+  {
+    "smoka7/hop.nvim",
+    keys = {
+      { "<leader>h", "<cmd>HopWord<cr>",    desc = "::Hop HopWord" },
+      { "<leader>p", "<cmd>HopPattern<cr>", desc = "::Hop HopPattern" }
+    },
+    config = true
+  },
 }
